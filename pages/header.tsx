24fbabd46bf3c8895f182f3af234fb19a14df788
Julio Formiga <julio.formiga@gmail.com>
2024-05-06 23:32:54 +0200

    add: header logo

--- pages/header.tsx
+++ pages/header.tsx
@@ -1,20 +1,40 @@
-import { LogoDev } from "@mui/icons-material";
-import { Button } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
 	return (
 		<header>
-			<nav className="w-full flex fixed top-0 z-50 py-6 px-24 text-center bg-green-800/60 backdrop-blur-md">
-				<div>Logo</div>
-				<div>
-					<Link href="/">
-						<Button>Init</Button>
-					</Link>
-					<Link href="/links">
-						<Button>Links</Button>
-					</Link>
-				</div>
+			<nav className="w-full flex fixed top-0 z-50 py-10 px-24 text-center bg-green-900/90 backdrop-blur-lg">
+				<Grid container>
+					<Grid item xs={4}>
+						<Link href="/">
+							<Button>Links</Button>
+						</Link>
+						<Link href="/links">
+							<Button>Links</Button>
+						</Link>
+					</Grid>
+					<Grid item xs={4}>
+						<Image
+							className="fixed content-center top-0"
+							alt="Logo"
+							src="/rickandmorty.png"
+							width="400"
+							height="155"
+							placeholder="empty"
+							priority={true}
+						/>
+					</Grid>
+					<Grid item xs={4}>
+						<Link href="/">
+							<Button>Links</Button>
+						</Link>
+						<Link href="/links">
+							<Button>Links</Button>
+						</Link>
+					</Grid>
+				</Grid>
 			</nav>
 		</header>
 	);
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 	return (
 		<div className={`${inter.className}`}>
 			<Head>
-				<title>Wodka42</title>
+				<title>W42 - Rick&#38;Morty</title>
 			</Head>
 			<Header />
 			<Main />

d6738baf4cd3c7cc634fac8bfbe38f0b94bb8678
Julio Formiga <julio.formiga@gmail.com>
2024-05-08 02:39:59 +0200

    wip: header

--- pages/header.tsx
+++ pages/header.tsx
@@ -1,48 +1,62 @@
-import { Button, Grid, Icon } from "@mui/material";
+import { Button, Grid, IconButton, InputBase, Paper } from "@mui/material";
 import Link from "next/link";
 import Image from "next/image";
 import { useTheme } from "next-themes";
 import { DarkMode, LightMode } from "@mui/icons-material";
+import SearchIcon from "@mui/icons-material/Search";
 
 export default function Header() {
-  const { systemTheme, theme, setTheme } = useTheme();
-  const currentTheme = theme === "system" ? systemTheme : theme;
-  return (
-    <header>
-      <nav className="w-full flex fixed top-0 z-50 py-10 px-24 text-center bg-green-900/90 backdrop-blur-lg">
-        <Grid container>
-          <Grid item xs={4}>
-            <Link href="/">
-              <Button>Links</Button>
-            </Link>
-            <Link href="/links">
-              <Button>Links</Button>
-            </Link>
-          </Grid>
-          <Grid item xs={4}>
-            <Image
-              className="fixed content-center top-0"
-              alt="Logo"
-              src="/rickandmorty.png"
-              width="400"
-              height="155"
-              placeholder="empty"
-              priority={true}
-            />
-          </Grid>
-          <Grid item xs={4}>
-            <Button
-              onClick={() =>
-                theme == "dark" ? setTheme("light") : setTheme("dark")
-              }
-              className="bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 px-2 py-2 md:text-xs rounded-lg text-xs"
-            >
-              <DarkMode className="visible dark:invisible" />
-              <LightMode className="invisible dark:visible" />
-            </Button>
-          </Grid>
-        </Grid>
-      </nav>
-    </header>
-  );
+	const { systemTheme, theme, setTheme } = useTheme();
+	const currentTheme = theme === "system" ? systemTheme : theme;
+	return (
+		<header>
+			<nav className="header">
+				<Grid container>
+					<Grid item md={4} className="text-left">
+						<Link href="/" className="pr-10">
+							<Button variant="contained">Index</Button>
+						</Link>
+						<Link href="/links">
+							<Button variant="contained">List</Button>
+						</Link>
+					</Grid>
+					<Grid item md={4} className="text-center content-center">
+						<Image
+							className="fixed content-center top-0"
+							alt="Logo"
+							src="/rickandmorty.png"
+							width="400"
+							height="133"
+							placeholder="empty"
+							priority={true}
+						/>
+					</Grid>
+					<Grid item md={4} className="text-right content-center">
+						<Paper
+							component="form"
+							className="bg-green-300 dark:bg-green-950 w-100"
+						>
+							<InputBase
+								className="text-black dark:text-white"
+								placeholder="Search"
+								inputProps={{ "aria-label": "search" }}
+							/>
+							<IconButton type="button" sx={{ p: "4px" }} aria-label="search">
+								<SearchIcon />
+							</IconButton>
+						</Paper>
+					</Grid>
+				</Grid>
+				<Button
+					className="darkModeSelector"
+					onClick={() =>
+						theme == "dark" ? setTheme("light") : setTheme("dark")
+					}
+				>
+					<DarkMode className="block dark:hidden" />
+					<LightMode className="hidden dark:block" />
+				</Button>
+			</nav>
+		</header>
+	);
 }
